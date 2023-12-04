6a35c37e50946badea163e2d6be50bf91ff562b3
KatrychAlina <katricalina5@gmail.com>
2023-12-04 14:02:26 +0100

    .

--- client/src/pages/Registration/registration.jsx
+++ client/src/pages/Registration/registration.jsx
@@ -4,7 +4,6 @@ import styles from './styles.module.css';
 import facebook from './images/facebook.png';
 import google from './images/google.png';
 import twitter from './images/twitter.png';
-
 import { useDispatch, useSelector } from 'react-redux'
 import { registerUser, checkIsAuth } from '../../redux/features/auth/authSlice'
 
@@ -12,37 +11,42 @@ function Registration() {
   const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [username, setUsername] = useState('')
+    const [passwordError, setPasswordError] = useState('');
 
     const dispatch = useDispatch()
 
     const handleSubmit = () => {
       try {
-          dispatch(registerUser({ email, password,username }))
-          setEmail('')
-          setPassword('')
-          setUsername('')
+        if (password.length < 5) {
+          setPasswordError('Пароль повинен містити щонайменше 5 символів');
+        } else {
+          setPasswordError('');   
+          dispatch(registerUser({ email, password, username }));
+          setEmail('');
+          setPassword('');
+          setUsername('');
+        }
       } catch (error) {
-          console.log(error)
+        console.log(error);
       }
-  }
-
+    };
+    
     return (
       <div className={styles.bodyBlock}>
         <div className={styles.oval}></div>
   
         <div className={styles.centeredСontent}>
           <h1 className={styles.registrationTitle}>Rejestracja</h1>
-  
           <form className={styles.form}>
             <input type="email" id="email" name="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-          </form>
+          </form>      
           <form className={styles.form}>
             <input type="password" id="password" name="password" placeholder="Hasło"  value={password} onChange={(e) => setPassword(e.target.value)}/>
           </form>
+          {passwordError && <p className={styles.errorText}>{passwordError}</p>}
           <form className={styles.form}>
             <input type="pseudonym" id="pseudonym" name="pseudonym" placeholder="Wymyśl pseudonim" value={username} onChange={(e) => setUsername(e.target.value)}/>
-          </form>
-  
+          </form> 
           <div className={styles.termsAndConditions}>
           <p>
             Kontynuując zgadzasz się na nasze{' '}
@@ -53,15 +57,11 @@ function Registration() {
           </div>
            <div className={styles.contentDown}>
           <Link to="/logging" className={styles.nextButton} onClick={handleSubmit}>Dalej</Link>
-  
           <div className={styles.lines}>
                  <div className={styles.line}></div>
                 <span className={styles.or}>LUB</span>
                 <div className={styles.line}></div>
          </div>
- 
-
-  
           <div className={styles.logoImages}>
             <img src={facebook} alt="Facebook" />
             <img src={google} alt="Google" />
@@ -79,6 +79,4 @@ function Registration() {
       </div>
     );
   }
-
-
 export default Registration;
